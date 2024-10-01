c7e6267f3c2de67e43ced9f3d3410049f12177e9
wilson <112340085+wilsnwei@users.noreply.github.com>
2024-10-01 10:24:32 -0400

    back end

---
+++ movies/src/main/java/com/wilsonmovies/movies/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.wilsonmovies.movies;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
